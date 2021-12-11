d9d463d3bce64e71ffe44b22b1b411ba7140fc6d
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2021-12-11 15:44:49 +0530

    Adding frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+      <Footer/>
+  </div>;
 }
 
 export default App;
