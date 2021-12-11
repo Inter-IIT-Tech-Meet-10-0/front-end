e938f7468989677ebb77dc236f96b62ef9d2e622
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2021-12-12 04:42:34 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Events from "./components/Events/Events";
 import Team from "./components/Team/Team";
 import Timeline from "./components/Timeline/Timeline";
+import Footer from "./components/Footer/Footer";
+import ComingSoon from "./components/ComingSoon/ComingSoon";
 import { Routes } from "react-router-dom";
 
 function App() {
@@ -14,12 +16,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ComingSoon />} />
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/Events" element={<Events />} />
           <Route path="/Team" element={<Team />} />
           <Route path="/Timeline" element={<Timeline />} />
-        </Routes>
+        </Routes> 
+         <Footer/>
       </Router>
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React from 'react'
+import Classes from './Footer.module.css';
 
-export function Footer() {
-  return <div>Footer</div>;
+// icons import
+import {IoMail} from 'react-icons/io5';
+import {BsTelephoneFill} from 'react-icons/bs';
+import {MdLocationOn} from 'react-icons/md';
+import {BsFacebook} from 'react-icons/bs'
+import {FaLinkedinIn} from 'react-icons/fa'
+import {GrInstagram} from 'react-icons/gr';
+import {IoMdSend} from 'react-icons/io';
+
+
+
+function Footer() {
+    return (
+        <div className={Classes.container}>
+             <div className={Classes.quickLinks}>
+                 {/* <div className={Classes.heading}>
+                     Quick Links
+                 </div> */}
+                 {/* <ul className={Classes.list1}>
+                    <li className={Classes.listItem}>Blah</li>
+                    <li className={Classes.listItem}>Blah</li>
+                    <li className={Classes.listItem}>Blah</li>
+                    <li className={Classes.listItem}>Blah</li>
+                    <li className={Classes.listItem}>Blah</li>
+                 </ul> */}
+             </div>
+             <div className={Classes.contactUs}>
+             <div className={Classes.headingContact}>
+                     Contact Us
+                 </div>
+                 <ul className={Classes.list}>
+                    <li className={Classes.listItem2}> <IoMail color= {'white'} /> <a className={Classes.text} href="mailto:contact@interiit-tech.org">contact@interiit-tech.org</a></li>
+                    <li className={Classes.listItem2}><BsTelephoneFill color={'white'}/><span className={Classes.text}>+91 1234567890</span> </li>
+                    <li className={Classes.listItem2}><MdLocationOn color={'white'}/><span className={Classes.text}> Kharagpur</span> </li>
+                 </ul>
+                 <ul className={Classes.socialList}>
+                     <li className={Classes.socialItem}><BsFacebook color={'white'} size={25}/></li>
+                     <li className={Classes.socialItem}><FaLinkedinIn color={'white'} size={25}/></li>
+                     <li className={Classes.socialItem}><GrInstagram color={'white'} size={25}/></li>
+                 </ul>
+             </div>
+             <div className={Classes.email}>
+                 <div className={Classes.heading2}>
+                     Reach Out To Us!
+                 </div>
+                 <form className={Classes.formContainer}>
+                     <input className={Classes.input} type='text'/>
+                     <button type='submit' className={Classes.button}><IoMdSend color={'white'} size={25}/></button>
+                 </form>
+             </div>
+        </div>
+    )
 }
+
+export default Footer
