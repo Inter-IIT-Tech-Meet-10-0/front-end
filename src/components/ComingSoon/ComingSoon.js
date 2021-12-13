d1a4588fdd4e9bd15beb82143ce5bccaa9bf4681
Shrinish Vhanbatte <56491104+shrinish123@users.noreply.github.com>
2021-12-13 18:48:17 +0530

    Adding responsiveness and removing navbar (#15)
    * Adding responsiveness and removing navbar
    
    * Add Gaurav's phone number
    
    Co-authored-by: Parth Paradkar <parthparadkar3@gmail.com>

---
+++ src/components/ComingSoon/ComingSoon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Classes from './ComingSoon.module.css';
+import logo from '../../images/logo.png';
+
+function ComingSoon() {
+    return (
+        <div className={Classes.background}>
+           <div className={Classes.centerContainer}>
+              <img className={Classes.logo} src={logo} alt='logo'/>
+              <div className={Classes.title}> INTER IIT TECH-MEET 10.0</div>  
+             <div className={Classes.subtitle}> IIT Kharagpur</div>  
+             <div className={Classes.text}> COMING SOON</div>  
+           </div>
+        </div>
+    )
+}
+
+export default ComingSoon;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,44 +5,44 @@ import Classes from './Footer.module.css';
 import {IoMail} from 'react-icons/io5';
 import {BsTelephoneFill} from 'react-icons/bs';
 import {MdLocationOn} from 'react-icons/md';
-import {BsFacebook} from 'react-icons/bs'
-import {FaLinkedinIn} from 'react-icons/fa'
-import {GrInstagram} from 'react-icons/gr';
-import {IoMdSend} from 'react-icons/io';
+// import {BsFacebook} from 'react-icons/bs'
+// import {FaLinkedinIn} from 'react-icons/fa'
+// import {GrInstagram} from 'react-icons/gr';
+// import {IoMdSend} from 'react-icons/io';
 
 
 
 function Footer() {
     return (
         <div className={Classes.container}>
-             <div className={Classes.quickLinks}>
-                 {/* <div className={Classes.heading}>
+             {/* <div className={Classes.quickLinks}>
+                 <div className={Classes.heading}>
                      Quick Links
-                 </div> */}
-                 {/* <ul className={Classes.list1}>
+                 </div>
+                 <ul className={Classes.list1}>
                     <li className={Classes.listItem}>Blah</li>
                     <li className={Classes.listItem}>Blah</li>
                     <li className={Classes.listItem}>Blah</li>
                     <li className={Classes.listItem}>Blah</li>
                     <li className={Classes.listItem}>Blah</li>
-                 </ul> */}
-             </div>
+                 </ul>
+             </div> */}
              <div className={Classes.contactUs}>
              <div className={Classes.headingContact}>
                      Contact Us
                  </div>
                  <ul className={Classes.list}>
                     <li className={Classes.listItem2}> <IoMail color= {'white'} /> <a className={Classes.text} href="mailto:contact@interiit-tech.org">contact@interiit-tech.org</a></li>
-                    <li className={Classes.listItem2}><BsTelephoneFill color={'white'}/><span className={Classes.text}>+91 1234567890</span> </li>
+                    <li className={Classes.listItem2}><BsTelephoneFill color={'white'}/><span className={Classes.text}>+91 9350031637</span> </li>
                     <li className={Classes.listItem2}><MdLocationOn color={'white'}/><span className={Classes.text}> Kharagpur</span> </li>
                  </ul>
-                 <ul className={Classes.socialList}>
+                 {/* <ul className={Classes.socialList}>
                      <li className={Classes.socialItem}><BsFacebook color={'white'} size={25}/></li>
                      <li className={Classes.socialItem}><FaLinkedinIn color={'white'} size={25}/></li>
                      <li className={Classes.socialItem}><GrInstagram color={'white'} size={25}/></li>
-                 </ul>
+                 </ul> */}
              </div>
-             <div className={Classes.email}>
+             {/* <div className={Classes.email}>
                  <div className={Classes.heading2}>
                      Reach Out To Us!
                  </div>
@@ -50,7 +50,7 @@ function Footer() {
                      <input className={Classes.input} type='text'/>
                      <button type='submit' className={Classes.button}><IoMdSend color={'white'} size={25}/></button>
                  </form>
-             </div>
+             </div> */}
         </div>
     )
 }
