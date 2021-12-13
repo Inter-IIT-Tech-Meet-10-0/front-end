e04e7eca6e218d1c449f33a7b0f9543627dc8128
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2021-12-13 17:37:50 +0530

    Adding responsiveness and removing navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/NavBar/NavBar";
-import Home from "./components/Home/Home";
+// import Navbar from "./components/NavBar/NavBar";
+// import Home from "./components/Home/Home";
+import ComingSoon from "./components/ComingSoon/ComingSoon";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Events from "./components/Events/Events";
 import Team from "./components/Team/Team";
@@ -13,9 +14,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ComingSoon />} />
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/Events" element={<Events />} />
           <Route path="/Team" element={<Team />} />
