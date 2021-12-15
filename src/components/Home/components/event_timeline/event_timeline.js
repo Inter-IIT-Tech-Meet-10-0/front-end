c092cb55979a69ff50c6ca33f1d7504efb3430c5
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2021-12-15 16:17:12 +0530

    Adding Event Timeline Component

--- src/components/Home/components/event_timeline/event_timeline.js
+++ src/components/Home/components/event_timeline/event_timeline.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React from 'react'
+import Classes from './event_timeline.module.css';
+import { Carousel } from "3d-react-carousal";
 
-export default function EventTimeline() {
-  return <div></div>;
+
+function EventTimeline() {
+
+    let slides = [<EventCard/>,<EventCard/>,<EventCard/>,<EventCard/>];
+
+
+
+    return (
+        <div className={Classes.container}>
+           <Carousel className={Classes.slides} slides={slides} />
+        </div>
+    )
 }
+
+function EventCard() {
+    return (
+        <div className={Classes.cardContainer}>
+           <div className={Classes.date}>27</div>
+           <div className={Classes.month}>November</div>
+            <div className={Classes.eventC}>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default EventTimeline
