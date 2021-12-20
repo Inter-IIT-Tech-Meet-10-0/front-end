9d48daf80582e69e5bbae2e86ed40b9c3084f481
AAYUSHAIRAN <63530479+AAYUSHAIRAN@users.noreply.github.com>
2021-12-20 15:15:09 +0530

    HOME -> About inter IIT component (#18)
    * Adding Home->Participants Folder
    
    * Adding HOME -> AboutInterIIT component

--- src/components/Home/components/participants/participants.js
+++ src/components/Home/components/participants/participants.js
@@ -1,5 +1,72 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import "./participants.css";
+import { list } from "./data";
 
 export default function Participants() {
-  return <div></div>;
+  let data = [];
+  const [pointer, setPointer] = useState([1, 7]);
+  useEffect(() => {
+    let timer = setTimeout(function () {
+      let new_pointer = [
+        pointer[0] + 1 > 23 ? (pointer[0] + 1) % 23 : pointer[0] + 1,
+        pointer[1] + 1 > 23 ? (pointer[1] + 1) % 23 : pointer[1] + 1,
+      ];
+      setPointer(new_pointer);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+  let start = pointer[0];
+  let end = pointer[1];
+  if (start < end) {
+    for (let i = start; i <= end; ++i) {
+      console.log(list[i - 1]);
+      data.push(list[i - 1]);
+    }
+  } else {
+    for (let i = start; i <= 23; ++i) {
+      data.push(list[i - 1]);
+    }
+    for (let i = 1; i <= end; ++i) {
+      data.push(list[i - 1]);
+    }
+  }
+  console.log(data[0]);
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <div className="HomeParticipants">
+        <div>
+          {data.map((data_val) => {
+            const { id, name } = data_val;
+            return <div key={id}>{name}</div>;
+          })}
+        </div>
+      </div>
+      <h1
+        style={{
+          fontFamily: "sans-serif",
+          fontStyle: "normal",
+          fontWeight: "600",
+          fontSize: "60px",
+
+          textAlign: "center",
+          color: "#FFFFFF",
+          backgroundColor: "black",
+          margin: "0px",
+          letterSpacing: "4px",
+        }}
+      >
+        Participants
+      </h1>
+      <div
+        style={{
+          margin: "0px 118px",
+          height: "171px",
+          borderBottom: "6px solid white",
+        }}
+      ></div>
+    </div>
+  );
 }
