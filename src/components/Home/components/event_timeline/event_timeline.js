431aec45067ea937199cb1bcbe7e51e541bc3ac3
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2021-12-21 00:11:17 +0530

    Adding required changes

--- src/components/Home/components/event_timeline/event_timeline.js
+++ src/components/Home/components/event_timeline/event_timeline.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import Classes from './event_timeline.module.css';
-import { Carousel } from "3d-react-carousal";
-
 
 function EventTimeline() {
 
-    let slides = [<EventCard/>,<EventCard/>,<EventCard/>,<EventCard/>];
-
-
-
     return (
-        <div className={Classes.container}>
-           <Carousel className={Classes.slides} slides={slides} />
+        <div className={Classes.mainContainer}>
+            <div className={Classes.container}>
+                <EventCard />
+                <EventCard />
+                <EventCard />
+            </div>
         </div>
     )
 }
 
 function EventCard() {
+    
     return (
-        <div className={Classes.cardContainer}>
-           <div className={Classes.date}>27</div>
+        <div className={ Classes.cardContainer}>
+           <div className={ Classes.date}>27</div>
            <div className={Classes.month}>November</div>
             <div className={Classes.eventC}>
                 <div className={Classes.eventName}>Event today: Cycle Trip</div>
