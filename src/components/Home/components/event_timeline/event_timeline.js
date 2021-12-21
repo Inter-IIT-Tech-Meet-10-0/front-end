7677036264b0a5642ccf27f3d7a17c681d3666fd
Shrinish Vhanbatte <56491104+shrinish123@users.noreply.github.com>
2021-12-21 20:11:44 +0530

    Adding Event Timeline Component (#20)
    * Adding Event Timeline Component
    
    * Adding required changes

--- src/components/Home/components/event_timeline/event_timeline.js
+++ src/components/Home/components/event_timeline/event_timeline.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React from 'react'
+import Classes from './event_timeline.module.css';
 
-export default function EventTimeline() {
-  return <div></div>;
+function EventTimeline() {
+
+    return (
+        <div className={Classes.mainContainer}>
+            <div className={Classes.container}>
+                <EventCard />
+                <EventCard />
+                <EventCard />
+            </div>
+        </div>
+    )
 }
+
+function EventCard() {
+    
+    return (
+        <div className={ Classes.cardContainer}>
+           <div className={ Classes.date}>27</div>
+           <div className={Classes.month}>November</div>
+            <div className={Classes.eventC}>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+                <div className={Classes.eventName}>Event today: Cycle Trip</div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default EventTimeline
