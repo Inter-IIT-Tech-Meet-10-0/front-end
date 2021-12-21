77b270c82efece425d95d596e7b0b72356eef81d
Parth <parthparadkar3@gmail.com>
2021-12-21 23:36:13 +0530

    Add Home component to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Navbar from "./components/NavBar/NavBar";
-// import Home from "./components/Home/Home";
-import ComingSoon from "./components/ComingSoon/ComingSoon";
+import Home from "./components/Home/Home";
+// import ComingSoon from "./components/ComingSoon/ComingSoon";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Events from "./components/Events/Events";
 import Team from "./components/Team/Team";
@@ -16,7 +16,7 @@ function App() {
       <Router>
         {/* <Navbar /> */}
         <Routes>
-          <Route path="/" element={<ComingSoon />} />
+          <Route path="/" element={<Home />} />
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/Events" element={<Events />} />
           <Route path="/Team" element={<Team />} />
