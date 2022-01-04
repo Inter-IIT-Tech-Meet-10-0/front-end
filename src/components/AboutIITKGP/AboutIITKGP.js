d8cd269f68219669c8864c27ecde6949f662c579
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2022-01-04 19:17:30 +0530

    Added the basic layout for About us Page

---
+++ src/components/AboutIITKGP/AboutIITKGP.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutIITKGP() {
+    return (
+        <div>
+            About IIT KGP
+        </div>
+    )
+}
+
+export default AboutIITKGP
---
+++ src/components/AboutTechMeet/AboutTechMeet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutTechMeet() {
+    return (
+        <div>
+            About Tech Meet
+        </div>
+    )
+}
+
+export default AboutTechMeet
