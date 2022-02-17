7848cb91a3a17284c011928be0524c606c71f7b1
Shrinish Vhanbatte <56491104+shrinish123@users.noreply.github.com>
2022-02-17 18:13:55 +0530

    Events Updated Design (#46)
    * Events Update
    
    * adding data.js and fising image import
    
    * Navbar improvement

---
+++ src/components/Events/data.js
@@ -0,0 +1,72 @@
+import event1 from '../../images/event1.png';
+
+export const lowPrep = [
+      {
+        name:'Event Name',
+        link :'',
+        image:event1
+      },
+      {
+        name:'Event Name',
+        link :'',
+        image:event1
+      },
+      {
+        name:'Event Name',
+        link :'',
+        image:event1
+      },
+      {
+        name:'Event Name',
+        link :'',
+        image:event1
+      },
+
+]
+export const midPrep = [
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+
+]
+
+export const highPrep = [
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+    {
+      name:'Event Name',
+      link :'',
+      image:event1
+    },
+
+]
