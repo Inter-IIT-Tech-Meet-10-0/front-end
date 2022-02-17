dc96bed96180bf7e13834edc19ee655444ded87e
aayush airan <ayushairan799@gmail.com>
2022-02-17 16:20:16 +0530

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import AboutInteriit from "./components/about_interiit/about_interiit";
 import Countdown from "./components/countdown/countdown";
 import EventTimeline from "./components/event_timeline/event_timeline";
 import Hero from "./components/hero/hero";
-// import LatestUpdates from "./components/latest_updates/latest_updates";
+import LatestUpdates from "./components/latest_updates/latest_updates";
 import Participants from "./components/participants/participants";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero />
       <Countdown />
       <EventTimeline />
-      {/* <LatestUpdates /> */}
+      <LatestUpdates />
       <AboutInteriit />
       <Participants />
     </React.Fragment>
