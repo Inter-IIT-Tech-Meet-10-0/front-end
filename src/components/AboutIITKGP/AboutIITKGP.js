2cf31febd0f4002c04043e4f2746979a8bd10684
Parth Paradkar <parthparadkar3@gmail.com>
2022-02-20 01:09:34 +0530

    Update AboutIITKGP.js

--- src/components/AboutIITKGP/AboutIITKGP.js
+++ src/components/AboutIITKGP/AboutIITKGP.js
@@ -26,11 +26,7 @@ function AboutIITKGP() {
             institute strives to be a centre of excellence in education and
             research, producing global leaders in science, technology and
             management, and was awarded the status of "Institute of Eminence" by
-            the government of India in 2019. With the motto "Yogah Karmasu
-            Kaushalam" the institute strives to be a centre of excellence in
-            education and research, producing global leaders in science,
-            technology and management, and was awarded the status of "Institute
-            of Eminence" by the government of India in 2019.
+            the government of India in 2019.
           </div>
           <div className={Classes.buttonContainer}>
             <div className={Classes.button}>
