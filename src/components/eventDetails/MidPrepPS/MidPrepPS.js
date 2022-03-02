037a6d3a939d87f1c7a429e86692e4794029e7a4
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2022-03-02 17:23:52 +0530

    Changes

---
+++ src/components/eventDetails/MidPrepPS/MidPrepPS.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Classes from './MidePrep.module.css';
+
+export const ISRO=()=> {
+  return (
+    <>
+    </>
+  )
+}
+export const DRDO=()=> {
+    return (
+      <>
+      </>
+    )
+}
+  
+export const Mudrex=()=> {
+    return (
+      <>
+      </>
+    )
+  
+}
+
+export const Bosch=()=> {
+    return (
+      <>
+      </>
+    )
+  }
+
